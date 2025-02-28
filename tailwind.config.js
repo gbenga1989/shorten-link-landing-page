c1721256ca39e2af15831947ba60abf9099df4c1
gbenga <odetoyinbog@gmail.com>
2025-02-28 13:17:07 +0100

    28-1

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      colors:{
+        Cyan: 'hsl(180, 66%, 49%)',
+        darkViolet: 'hsl(257, 27%, 26%)',
+        Red: 'hsl(0, 87%, 67%)',
+        Gray: 'hsl(0, 0%, 75%)',
+        GrayishViolet: 'hsl(257, 7%, 63%)',
+        VeryDarkBlue: 'hsl(255, 11%, 22%)',
+        VeryDarkViolet: 'hsl(260, 8%, 14%)'
+      },
+      fontFamily:{
+        sans:['poppins', 'sans-serif']
+      },
+      spacing: {
+        180:'32rem'
+      }
+    },
+  },
+  plugins: [],
+}
