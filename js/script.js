6f879a148fba8e4a41b4b0d987ad228c67f6312b
gbenga <odetoyinbog@gmail.com>
2025-03-01 14:46:20 +0100

    1-7

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 const input = document.getElementById('link-input')
 const linkForm = document.getElementById('link-Form')
 const errMsg = document.getElementById('err-msg')
+
+
+linkForm.addEventListener('submit', formSubmit)
+
+function formSubmit(e){
+  e.preventDefault()
+
+  console.log(123);
+}
