fbe2f85880c6481ec5f8ee0215fb32ee51553167
gbenga <odetoyinbog@gmail.com>
2025-03-05 12:59:46 +0100

    5-7

--- js/script.js
+++ js/script.js
@@ -1,11 +1,21 @@
+const btn = document.getElementById('menu-btn')
+const menu = document.getElementById('menu')
+
 const input = document.getElementById('link-input')
 const linkForm = document.getElementById('link-form')
 const errMsg = document.getElementById('err-msg')
 
-
+btn.addEventListener('click', navToggle)
 linkForm.addEventListener('submit', formSubmit)
 
-// validate a url
+// Toggle Mobile Menu
+function navToggle() {
+  btn.classList.toggle('open')
+  menu.classList.toggle('flex')
+  menu.classList.toggle('hidden')
+}
+
+// Validate a URL
 function validURL(str) {
   var pattern = new RegExp(
     '^(https?:\\/\\/)?' + // protocol
@@ -19,20 +29,18 @@ function validURL(str) {
   return !!pattern.test(str)
 }
 
-
-function formSubmit(e){
-
+function formSubmit(e) {
   e.preventDefault()
 
-  if (input.value === ''){
-    errMsg.innerHTML = 'please enter something';
-    input.classList.add('border-red' )
-  } else if(!validURL(input.value)) {
-    errMsg.innerHTML = 'please enter a balid Url';
-    input.classList.add('border-red' )
+  if (input.value === '') {
+    errMsg.innerHTML = 'Please enter something'
+    input.classList.add('border-red')
+  } else if (!validURL(input.value)) {
+    errMsg.innerHTML = 'Please enter a valid URL'
+    input.classList.add('border-red')
   } else {
-     errMsg.innerHTML = '';
-    input.classList.remove('border-red' )
-    alert('success')
+    errMsg.innerHTML = ''
+    input.classList.remove('border-red')
+    alert('Success')
   }
 }
